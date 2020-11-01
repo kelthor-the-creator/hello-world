fb66db9787ca816632ab143b70b720ae11afac28
kelthor-the-creator <57299270+kelthor-the-creator@users.noreply.github.com>
2020-11-01 16:18:55 +0000

    Delete csv_reader.py

--- csv_reader.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("Data/Video_Games_Sales_as_at_22_Dec_2016.csv") as file:
-    csv_object= csv.reader(file)
-    for row in csv_object:
-                print("CSV row: {0}".format(row))
